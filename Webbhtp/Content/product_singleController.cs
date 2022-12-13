da8f0f791b4937dda7df53cdfd74f3817c0cbaa8
Tungunuong <daoviettu26@gmail.com>
2022-12-14 00:57:37 +0700

    up

---
+++ Webbhtp/Content/product_singleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webbhtp.Content
+{
+    public class product_singleController : Controller
+    {
+        // GET: product_single
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
