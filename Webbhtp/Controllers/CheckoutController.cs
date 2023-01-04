a5220c4d2736d32f6fcc33f7b906f72a24dcc884
Tungunuong <daoviettu26@gmail.com>
2023-01-04 20:38:43 +0700

    up

---
+++ Webbhtp/Controllers/CheckoutController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webbhtp.Content
+{
+    public class CheckoutController : Controller
+    {
+        BanHangThucPhamEntities myObj = new BanHangThucPhamEntities();
+        // GET: Checkout
+        public ActionResult Index()
+        {   if (Session["idUser"].Equals(" "))
+            {
+                return RedirectToAction("Index", "Shop");
+            } else
+            {
+                int userid = (int)Session["idUser"];
+                List<tb_Cart> item = myObj.tb_Cart.Where(i => i.UserId == userid).ToList();
+                if (item.Count > 0)
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Shop");
+                }
+            }
+            
+
+        }
+        [HttpPost]
+        public ActionResult Checkout(FormCollection frm)
+        {
+            int userid = (int)Session["idUser"];
+            tb_User user = myObj.tb_User.Find(userid);
+            List<tb_Cart> item = myObj.tb_Cart.Where(i => i.UserId == userid).ToList();
+            string username = user.FirstName + user.LastName;
+            tb_Order order = new tb_Order()
+            {
+                UserId = userid,
+                Code = frm["Code"],
+                CustomerName = frm["CustomerName"],
+                Phone = frm["Phone"],
+                Email = frm["Email"],
+                Address = frm["Address"],
+                CreatedDate = DateTime.Now,
+                CreatedBy = username,
+            };
+            myObj.tb_Order.Add(order);
+            myObj.SaveChanges();
+
+            foreach (var items in item)
+            {
+                tb_OrderDetail tb_OrderDetail = new tb_OrderDetail()
+                {
+                    OrderId = order.OrderId,
+                    ProductId = items.ProductId,
+                    Quantity = items.Quantity,
+                };
+                myObj.tb_OrderDetail.Add(tb_OrderDetail);
+                myObj.tb_Cart.Remove(items);
+                myObj.SaveChanges();
+            }
+            return View("Success");
+
+
+
+        }
+
+
+    }
+}
---
+++ Webbhtp/Controllers/HomeController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webbhtp.Controllers
+{   
+    public class HomeController : Controller
+    {   
+        private BanHangThucPhamEntities db = new BanHangThucPhamEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Slides = new Slidehome().ListAll();
+            ViewBag.Producthome = new Producthome().ListAll();
+            return View();
+        }
+
+       
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        BanHangThucPhamEntities lab = new BanHangThucPhamEntities();
+        public ActionResult TopMenu()
+        {
+            var item = lab.tb_Menu.Where(i => i.IsActive).ToList();
+            if (!Session["idUser"].Equals(" "))
+            { int id = (int)Session["idUser"];
+                TempData["cartt"] = lab.tb_Cart.Where(j => j.UserId == id).Count();
+            }
+            return PartialView("TopMenu", item);
+        }
+
+        //GET: Register
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(tb_User _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = db.tb_User.FirstOrDefault(s => s.Email == _user.Email);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    _user.IsAdmin = false;
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.tb_User.Add(_user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                var f_password = GetMD5(password);
+                var data = db.tb_User.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {   
+                    if(data.FirstOrDefault().IsAdmin == true)
+                    {
+                        Session["idUser"] = data.FirstOrDefault().UserID;
+                        return Redirect("~/Admin/Home/Index");
+                    } else
+                    {
+                        Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                        Session["idUser"] = data.FirstOrDefault().UserID;
+                        return RedirectToAction("Index");
+                    }
+                    //add session
+                   
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session["idUser"] = " ";
+            return RedirectToAction("Index");
+        }
+
+    }
+
+}
